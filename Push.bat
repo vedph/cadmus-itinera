8df04cc734e2c74785a67ae93be516b11cea5e93
Myrmex <dfusi@hotmail.com>
2023-06-23 18:21:11 +0200

    Update Push.bat

--- Push.bat
+++ Push.bat
@@ -4,8 +4,8 @@ prompt
 set nu=C:\Exe\nuget.exe
 set src=-Source https://api.nuget.org/v3/index.json
 
-%nu% push .\Cadmus.Itinera.Parts\bin\Debug\*.nupkg %src%
-%nu% push .\Cadmus.Itinera.Services\bin\Debug\*.nupkg %src%
-%nu% push .\Cadmus.Seed.Itinera.Parts\bin\Debug\*.nupkg %src%
+%nu% push .\Cadmus.Itinera.Parts\bin\Debug\*.nupkg %src% -SkipDuplicate
+%nu% push .\Cadmus.Itinera.Services\bin\Debug\*.nupkg %src% -SkipDuplicate
+%nu% push .\Cadmus.Seed.Itinera.Parts\bin\Debug\*.nupkg %src% -SkipDuplicate
 echo COMPLETED
 echo on
